9f4eee0183df2f9b256226c53dc001d9f6a6e929
Jayleaf1125 <Jasonpadilla104@gmail.com>
2021-11-16 15:37:56 +0000

    Code Challenge 16

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //write your code here
+const replaceWithYerr = (arr, start = 0, end = arr.length - 1) => {
+    let replaced = [];
+    for (let i = 0; i < arr.length; i++) {
+        // replaced += arr;
+        if (i >= start && i < start + end) {
+            replaced.push("yerr")
+        }
+        else {
+            replaced.push(arr[i]);
+        }
+    }
+    return replaced;
+}
+
+const months = ['Jan', 'February', 'March', 'April', 'May', 'June'];
+
+console.log(replaceWithYerr(months)) 
